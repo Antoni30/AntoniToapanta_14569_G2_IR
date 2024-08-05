995d6d239f31e34c619c3952d0c1310763c8bbb5
Antoni30 <artoapanta3@espe.edu.ec>
2024-08-05 09:42:06 -0500

    doc_Prueba

--- 03_Pruebas_Aceptacion/features/Page_Main.feature
+++ 03_Pruebas_Aceptacion/features/Page_Main.feature
@@ -2,19 +2,25 @@ Feature: Pagina main
 
    Scenario: Al seleccionar el boton ver se dirigira a ala pagina de About Us
         Given Estoy en la pagina principal 
+        Then genero un PDF "About Us"
         When selecciones el boton de ver mas me dirige a la pagina del About Us
+       
     
     Scenario: Al seleccionar el boton ver se dirigira a ala pagina Servicios
         Given Estoy en la pagina principal 
+        Then genero un PDF "Servicios"
         When selecciones el boton de ver mas me dirige a la pagina del Servicios
-
+        
     Scenario: Al seleccionar el boton ver se dirigira a ala pagina Productos
         Given Estoy en la pagina principal 
+        Then genero un PDF "Productos"
         When selecciones el boton de ver mas me dirige a la pagina del Productos
-
+      
     Scenario: Al seleccionar el boton ver se dirigira a ala pagina Proyectos
         Given Estoy en la pagina principal 
+        Then genero un PDF "Proyecto"
         When selecciones el boton de ver mas me dirige a la pagina del Proyectos
+       
 
     
 
--- 03_Pruebas_Aceptacion/features/step_definitions/steps_page_main.js
+++ 03_Pruebas_Aceptacion/features/step_definitions/steps_page_main.js
@@ -1,9 +1,14 @@
-const { Given,When,After } = require('@cucumber/cucumber');
+const { Given, When, Then ,AfterAll,After } = require('@cucumber/cucumber');
 const { Builder, By, Browser } = require('selenium-webdriver');
+const { PDFDocument, rgb } = require('pdf-lib');
+const fs = require('fs');
+const path = require('path');
 
 
 let driver;
-
+const screenshots = [];
+let pdfDoc;
+let pageIndex = 0;
 
 Given('Estoy en la pagina principal', async function () {
     driver = await new Builder().forBrowser(Browser.CHROME).build();
@@ -30,5 +35,65 @@ When('selecciones el boton de ver mas me dirige a la pagina del Proyectos', asyn
 After(async function () {
     if (driver) {
         await driver.quit();
+    }
+});
+
+
+Then('genero un PDF {string}', async function (escenario) {
+
+    if (!pdfDoc) {
+      pdfDoc = await PDFDocument.create();
+  }
+
+
+  // Tomar una captura de pantalla con Selenium
+  const screenshotPath = path.join(__dirname, `screenshot_${pageIndex}.png`);
+  const screenshot = await driver.takeScreenshot();
+  fs.writeFileSync(screenshotPath, Buffer.from(screenshot, 'base64'));
+
+  screenshots.push(screenshotPath);
+
+  // Agregar una nueva página al PDF
+  const page = pdfDoc.addPage([600, 800]);
+
+  // Agregar texto al PDF
+  page.drawText(`Captura de pantalla para el escenario ${pageIndex + 1}`, {
+      x: 50,
+      y: 720,
+      size: 11,
+      color: rgb(0, 0, 0.8),
+  });
+
+  page.drawText(`${escenario}`, {
+      x: 50,
+      y: 690,
+      size: 11,
+      color: rgb(0, 0, 0.8),
+  });
+
+  // Incrustar la imagen en el PDF
+  const pngImageBytes = fs.readFileSync(screenshotPath);
+  const pngImage = await pdfDoc.embedPng(pngImageBytes);
+  const pngDims = pngImage.scale(0.4);
+
+  page.drawImage(pngImage, {
+      x: 50,
+      y: 250,
+      width: pngDims.width,
+      height: pngDims.height,
+  });
+  pageIndex++;
+});
+
+AfterAll(async function () {
+    if (pdfDoc) {
+        // Guardar el PDF en el sistema de archivos
+        const pdfBytes = await pdfDoc.save();
+        fs.writeFileSync(path.join(__dirname, 'documento_Pruebas_Main_Page.pdf'), pdfBytes);
+
+        // Eliminar las capturas de pantalla temporales
+        for (const screenshot of screenshots) {
+            fs.unlinkSync(screenshot);
+        }
     }
 });
